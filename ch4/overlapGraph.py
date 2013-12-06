1b13ff79727b0cc033d99e951763d37125390509
biomystery <zhangc518@gmail.com>
2013-12-06 09:56:42 -0800

    upload overlayGraph
    Things Can be Done: Use a more efficent way to iterate through the two
    kmers lists.

--- ch4/overlapGraph.py
+++ ch4/overlapGraph.py
@@ -18,50 +18,55 @@ def overlapGraph(kmers):
 
     ----------
     Definition: 
-    prefix and suffix to refer to the first k − 1 nucleotides and last k
-    − 1 nucleotides of a k-mer, respectively. 
+    prefix and suffix to refer to the first k - 1 nucleotides and last k
+    - 1 nucleotides of a k-mer, respectively. 
 
     if prefix(kmer2) = suffix(kmer1), then kmer1 -> kmer2
 
-        
     node for each k-mer in Patterns and connect k-mers Pattern1 and Pattern2
     by a directed edge if the suffix of Pattern1 is equal to the prefix
     of Pattern2. The resulting graph is called the overlap graph on
     these k-mers, denoted Overlap(Patterns). 
     '''
-        
-    return kmers
+    # duplicate kmers
+    kmers_1 = kmers[:]
+
+    output = []
+    # use turple 
+    for kmer in kmers:
+        for kmer1 in kmers_1:
+            if suffix(kmer) == prefix(kmer1):
+                output.append((kmer,kmer1))
+
+    return sorted(output)
 
 def prefix(kmer):
     '''
-    prefix and suffix to refer to the first k − 1 nucleotides and last k
-    − 1 nucleotides of a k-mer, respectively. 
+    return the first k - 1 nucleotides of a k-mer, respectively.
     '''  
     return kmer[:-1]
   
-def prefix(kmer):
+def suffix(kmer):
     '''
-    prefix and suffix to refer to the first k − 1 nucleotides and last k
-    − 1 nucleotides of a k-mer, respectively. 
+    return the last k - 1 nucleotides of a k-mer, respectively. 
     '''  
     return kmer[1:]
 
 if __name__ == "__main__":
     # read file and get parameters
-    fin = open('c_input.txt','r')
-    k = int(fin.readline().rstrip('\n') )
-    Dna = fin.readline().rstrip('\n')
+    fin = open('og_input.txt','r')
+    kmers = [line.rstrip('\n') for line in fin.readlines()]
     fin.close()
+
     # run the function 
-    kmers = composition(k,Dna)
+    graph_tuple_list = overlapGraph(kmers)
     
     # output the results
-    with  open('c_output.txt','w') as fout:
-        for s in kmers:
-            fout.write ("%s\n" % s )
-            
-            from subprocess import call
-            call(["open","c_output.txt"])        
+    with  open('og_output.txt','w') as fout:
+        fout.write ('\n'.join("%s -> %s" % s for s in graph_tuple_list))
+
+    from subprocess import call
+    call(["open","og_output.txt"])        
             
             
             
