370e8d275ea5082cab5228b572cc833846595a31
biomystery <zhangc518@gmail.com>
2013-12-23 15:22:14 -0800

    Longest path in DAG

--- ch6/LongestPathInDAG.py
+++ ch6/LongestPathInDAG.py
@@ -1,18 +1,54 @@
-def LongestPathInDAG(source, sink, DAG,result):
+def LongestPathInDAG(source, sink, DAG, seq):
     '''
     S_a = max_{all predecessors b of node a}(S_b + weight of edge from b to a)
-    '''
-    pre_list = getPredecessors(a,DAG) 
 
+    Sample Input:
+     0
+     4
+     0->1:7
+     0->2:4
+     2->3:2
+     1->4:1
+     3->4:3
+
+     s(0) = 0
+     s(1) = 7
+     s(2) = 4
+     s(3) = s(2) + 2
+     s(4) = max(s(3) + 3, s(1) + 1) 
+
+     Sample Output:
+     9
+     0->2->3->4
+     '''
+    result = []
+    if source == sink:
+        return 0
+
+    pre_list = getPredecessors(sink,DAG) 
+    if len(pre_list) ==0:
+        return -999999
 
     for b in pre_list:
-        result += LongestPathInDAG(source,b,DAG) + getWeight(b,a,DAG)
-        return max(result)
+        #        print b
+        #raw_input('wait')
+        c = LongestPathInDAG(source,b,DAG,seq)
+        d = getWeight(b,sink,DAG)
+        #seq[b] = c
+        
+        result += [c + d]
+        #        print c,d,sink,result
+        
+    seq[sink] =pre_list[max(enumerate(result),key=lambda x: x[1])[0]]
+    return max(result)
     
 def getPredecessors(a,DAG):
     return [DAG[i][0] for i in range(len(DAG)) if DAG[i][1] ==a]
 
 def getWeight(b,a,DAG):
+    '''
+    weight for edge from b->a
+    '''
     for i in range(len(DAG)):
         if DAG[i][:2] == [b,a]:
             return DAG[i][2]
@@ -32,12 +68,24 @@ if __name__ == "__main__":
 
     
     # run the function 
-    result = []
-    LongestPathInDAG(source,sink,DAG,result)
+    seq = {}
+    output = LongestPathInDAG(source,sink,DAG,seq)
+    result =[]
+
+    tmp =sink
+    while tmp != source:
+        result.append(tmp)
+        tmp = seq[tmp]
+        
+    result.append(source)
+    result = '->'.join([str(i) for i in result[-1::-1] ])
+    #print result
+
     
     # output the results
     with  open('lpid_output.txt','w') as fout:
-        fout.write ("%d\n" % output)
+        fout.write('%d\n'% output)
+        fout.write ( result)
 
     from subprocess import call
     call(["open","lpid_output.txt"])        
