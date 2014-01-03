803cf0f27815bc6cad664d1ce62662c2c802fb91
biomystery <zhangc518@gmail.com>
2014-01-03 09:08:28 -0800

    fitting alignment
    Bug: Too slow.

--- ch7/fittingAlignment.py
+++ ch7/fittingAlignment.py
@@ -1,4 +1,6 @@
-def fittingAlignment():
+import globalAlign as ga
+
+def fittingAlignment(v,w):
     '''
     Fitting Alignment Problem: Construct a highest-scoring fitting
     alignment between two strings. 
@@ -20,8 +22,8 @@ def fittingAlignment():
     TAGGCTTA
     TAGA--TA
 
-    ¡°Fitting¡± w to v requires finding a substring v¡ä of v that
-    maximizes an alignment score the global alignment score between v¡ä
+    "Fitting" w to v requires finding a substring v' of v that
+    maximizes an alignment score the global alignment score between v'
     and w among all substrings of v. For example, the best global,
     local, and fitting alignments of v = GTAGGCTTAAGGTTA and w = TAGATA
     are shown below (with mismatch and indel penalties equal to 1). 
@@ -30,20 +32,49 @@ def fittingAlignment():
     alignment. On the other hand, the score of the optimal global
     alignment (-3) is smaller than that of the best fitting alignment
     (+2). 
-    
     '''
+    v_subs = getSubstring(v,w)
+    sigma = 1
+    #results = map(lambda v_sub: ga.globalAlign(w,v_sub,sigma),v_subs)
+    best_score, best_align  = -555, []
+    for idx, v_sub in enumerate(v_subs):
+        if idx % 100 == 0:
+            print str(idx) + '/' + str(len(v_subs))
+        align_tmp, score_tmp = ga.globalAlign(v_sub,w,sigma)
+        if score_tmp > best_score:
+            best_score = score_tmp
+            best_align = align_tmp
+
+    return best_align, best_score
+
+# all substrings:
+# http://stackoverflow.com/questions/127704/algorithm-to-return-all-combinations-of-k-elements-from-n
+# Thanks to Claudiu, recursive solution
+def subStringLen(str, length):
+    return [str[i:i+length] for i in range(len(str)-length+1)]
+
+
+def getSubstring(v,w):
+    output = []
+    for i in range(len(w),int(1.5*len(w))+1):
+        output += subStringLen(v,i) 
+    return list(set(output))
+     
+    
 if __name__ == "__main__":
     # read file and get parameters
     with open('fa_input.txt','r') as fin:
-        s = fin.readline().rstrip('\n') 
-        t = fin.readline().rstrip('\n') 
+        v = fin.readline().rstrip('\n') 
+        w = fin.readline().rstrip('\n') 
     
     # run the function 
-    distance = fittingAlignment(s,t)
+    align, score = fittingAlignment(v,w)
     
     # output the results
     with  open('fa_output.txt','w') as fout:
-        fout.write ("%d\n" % distance)
+        fout.write("%d\n" % score)
+        fout.write ("%s\n" % align[0])
+        fout.write ("%s\n" % align[1])
 
     from subprocess import call
     call(["open","fa_output.txt"])        
